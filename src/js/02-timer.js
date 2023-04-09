943c8f7c9b099e6695aa8ed287141bbef5ea4f76
Unn2007 <unn2007@i.ua>
2023-04-10 00:38:27 +0300

    timer_upd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,24 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const startBtn = document.querySelector('button[data-start]');
 const daysCounterValue = document.querySelector("span[data-days]");
 const hoursCounterValue = document.querySelector("span[data-hours]");
 const minutesCounterValue = document.querySelector("span[data-minutes]");
 const secondsCounterValue = document.querySelector("span[data-seconds]");
+const inputDateTime = document.querySelector("#datetime-picker");
 startBtn.setAttribute('disabled', '');
 let currentTime = new Date();
 let selectedTime = null;
 let timerId=null;
 let secondsCounter=0;
+function addLeadingZero(value) {
+  
+  return  value<10 ? (String(value).padStart(2,"0")) :  String(value);
+}
+
+
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -22,13 +31,14 @@ if (currentTime.getTime()<selectedDates[0].getTime()) {
     selectedTime = selectedDates[0];
     startBtn.removeAttribute('disabled', '');
     }
-    else {window.alert("Please choose a date in the future");};
+    else {Notify.failure('Please choose a date in the future',{timeout: 2000,});};
     },
   };
   flatpickr("#datetime-picker", options);
   startBtn.addEventListener("click",onStart);
   function onStart() {
     startBtn.setAttribute('disabled', '');
+    inputDateTime.setAttribute('disabled', '');
     currentTime = new Date();
     timerId = setInterval(startCounter,1000);
     };
@@ -38,17 +48,20 @@ if (currentTime.getTime()<selectedDates[0].getTime()) {
   NumberOfMiliSeconds = selectedTime.getTime()-currentTime.getTime();
   NumberOfSeconds = Math.floor(NumberOfMiliSeconds/1000)-secondsCounter;
   NumberOfdays = Math.floor(NumberOfSeconds/86400);
-  NumberOfHours = Math.floor((NumberOfSeconds-NumberOfdays*86400)/3600);
-  NumberOfMinutes = Math.floor(((NumberOfSeconds-NumberOfdays*86400)-NumberOfHours*3600));
-
+  NumberOfHoursInLastDay = Math.floor((NumberOfSeconds-NumberOfdays*86400)/3600);
+  NumberOfMinutesInLastHours = Math.floor(((NumberOfSeconds-NumberOfdays*86400)-NumberOfHoursInLastDay*3600)/60);
+  NumberOfSecondsInLastMinutes = NumberOfSeconds - NumberOfdays*24*60*60-NumberOfHoursInLastDay*60*60 - NumberOfMinutesInLastHours*60;
+  daysCounterValue.textContent = addLeadingZero(NumberOfdays);
+  hoursCounterValue.textContent  = addLeadingZero(NumberOfHoursInLastDay);
+  minutesCounterValue.textContent = addLeadingZero(NumberOfMinutesInLastHours) ;
+  secondsCounterValue.textContent = addLeadingZero(NumberOfSecondsInLastMinutes);
+  if ((NumberOfdays===0)&(NumberOfHoursInLastDay===0)&(NumberOfMinutesInLastHours===0)&(NumberOfSecondsInLastMinutes===0)) {
     
-     daysCounterValue.textContent = NumberOfdays;
-     console.log(secondsCounter);
-     console.log((selectedTime.getDate()-currentTime.getDate()));
-     hoursCounterValue.textContent  = NumberOfHours;
-    // minutesCounterValue.textContent = selectedTime.getDate()-currentTime.getDate();
-    // secondsCounterValue.textContent = (selectedTime.getTime()-currentTime.getTime())/1000;
-
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', '');
+    inputDateTime.removeAttribute('disabled', '');
+  }
+  
 
  };
 
