9c614be23916015e34b9260f25875cb5ae75d396
Unn2007 <unn2007@i.ua>
2023-04-11 08:17:53 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,28 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+let delayPromise;
+let stepPromise;
+let amountPromise;
+const refs =
+{
+inputFirstDelay:document.querySelector('input[name="delay"]'),
+inputStep:document.querySelector('input[name="step"]'),
+inputAmount:document.querySelector('input[name="amount"]'),
+submitForm:document.querySelector('.form'),
+submitButton:document.querySelector('.form button'),
+};
+refs.submitForm.addEventListener("submit",onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  delayPromise=refs.inputDelay.value;
+  stepPromise=refs.inputStep.value;
+  amountPromise=refs.inputAmount.value;
+  refs.submitButton.setAttribute('disabled', '');
+};
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
