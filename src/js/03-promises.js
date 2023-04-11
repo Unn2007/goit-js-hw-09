6063dc9a55ede3f29fb2ecc900c2aa4f22f7b6e0
Unn2007 <unn2007@i.ua>
2023-04-11 23:56:32 +0300

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,37 +6,38 @@ let delayCount = 0;
 let delayArr = [];
 const refs =
 {
-inputFirstDelay:document.querySelector('input[name="delay"]'),
-inputStep:document.querySelector('input[name="step"]'),
-inputAmount:document.querySelector('input[name="amount"]'),
-submitForm:document.querySelector('.form'),
-submitButton:document.querySelector('.form button'),
+  inputFirstDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+  submitForm: document.querySelector('.form'),
+  submitButton: document.querySelector('.form button'),
 };
-refs.submitForm.addEventListener("submit",onSubmit);
+refs.submitForm.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  delayPromise=Number(refs.inputFirstDelay.value);
-  stepPromise=Number(refs.inputStep.value);
-  amountPromise=Number(refs.inputAmount.value);
+  delayPromise = Number(refs.inputFirstDelay.value);
+  stepPromise = Number(refs.inputStep.value);
+  amountPromise = Number(refs.inputAmount.value);
   refs.submitButton.setAttribute('disabled', '');
-  fillDelArr(delayPromise, amountPromise);
-  delayArr.map((delay, position)=>{
+  fillDelArr(delayPromise, amountPromise,stepPromise);
+  console.log(fillDelArr);
+  delayArr.map((delay, position,) => {
 
-createPromise((position+1), delay)
-  .then(({ position, delay }) => {
-    
-    Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-    }); 
+    createPromise((position + 1), delay)
+      .then(({ position, delay }) => {
+
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   });
   delayPromise = 0;
   stepPromise = 0;
-  amountPromise=0;
+  amountPromise = 0;
   delayCount = 0;
   delayArr = [];
   refs.submitButton.removeAttribute('disabled', '');
@@ -46,28 +47,28 @@ createPromise((position+1), delay)
 
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve,reject) => {
+  const promise = new Promise((resolve, reject) => {
 
     const shouldResolve = Math.random() > 0.3;
     console.log(shouldResolve);
-    setTimeout(()=>{
+    setTimeout(() => {
       if (shouldResolve) {
-        resolve({position:position,delay:delay});
+        resolve({ position: position, delay: delay });
       } else {
-        reject({position:position,delay:delay});
+        reject({ position: position, delay: delay });
       }
-    },delay);
-    
+    }, delay);
+
 
   })
   return promise;
 }
 
-function fillDelArr(delay, amount) {
-for (let positionPromise = 1; positionPromise <= amount; positionPromise+=1) {
-delayCount = (positionPromise === 1) ? delay : delayCount+=delay;
-delayArr.push(delayCount);
-}
+function fillDelArr(delay, amount,step) {
+  for (let positionPromise = 1; positionPromise <= amount; positionPromise += 1) {
+    delayCount = (positionPromise === 1) ? delay : delayCount += step;
+    delayArr.push(delayCount);
+  }
 };
 
 
